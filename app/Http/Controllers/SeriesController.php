c32f84aaf6713c7f5fab42e4357a9a414a4a46ba
vitor-frnds <vitor.fernandes@estudante.ufjf.br>
2023-08-08 01:09:18 -0300

    Finalizando o curso Laravel: Criando uma Aplicação com MVC - Alura

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+use GuzzleHttp\Psr7\Query;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Serie;
+use PHPUnit\Framework\MockObject\Rule\Parameters;
+use Illuminate\Database\Eloquent\Model;
 
 class SeriesController extends Controller
 {
@@ -11,11 +17,7 @@ class SeriesController extends Controller
     {
 
         // Lista de séries
-        $series = [
-            'Peaky Blinders',
-            'The Witcher',
-            'Breaking Bad'
-        ];
+        $series = Serie::query()->orderBy('nome')->get();
 
         // compact é exatamente a mesma coisa que está dentro dos colchetes
         //return view('listar-series', [
@@ -31,4 +33,14 @@ class SeriesController extends Controller
     {
         return view('series.create');
     } 
+
+    public function store(Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+        $serie = new Serie();                  //Temos uma classe que representa uma Série
+        $serie->nome = $nomeSerie;
+        $serie->save();
+
+        return redirect('/series');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,4 @@ Route::get('/', function () {
 // Primeiro item é a classe, o segundo é o nome do método que será chamado
 Route::get('/series', [SeriesController::class, 'index']);
 Route::get('/series/criar', [SeriesController::class, 'create']);
+Route::post('/series/salvar', [SeriesController::class, 'store']);
